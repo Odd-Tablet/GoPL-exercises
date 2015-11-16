876b3e8dc83536d38ef853c305da3051c2f5182c
Robert <qberting@yahoo.com>
2015-11-16 16:19:01 -0500

    Added Exercise 3.4

--- ch_03/surface/main.go
+++ ch_03/surface/main.go
@@ -5,12 +5,16 @@
 //!+
 
 // Surface computes an SVG rendering of a 3-D surface function.
+// Updated program to implement as web service + remove NaN/infinite values
 package main
 
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"math"
+	"net/http"
 )
 
 const (
@@ -23,26 +27,6 @@ const (
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
-
-func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay, er1 := corner(i+1, j)
-			bx, by, er2 := corner(i, j)
-			cx, cy, er3 := corner(i, j+1)
-			dx, dy, er4 := corner(i+1, j+1)
-			// Doesn't print if polygon contains infinite/NaN points
-			if er1 == nil && er2 == nil && er3 == nil && er4 == nil {
-				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-					ax, ay, bx, by, cx, cy, dx, dy)
-			}
-		}
-	}
-	fmt.Println("</svg>")
-}
 
 func corner(i, j int) (float64, float64, error) {
 	var err error
@@ -66,6 +50,35 @@ func corner(i, j int) (float64, float64, error) {
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
 	return math.Sin(r) / r
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	svg(w)
+}
+func svg(out io.Writer) {
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, er1 := corner(i+1, j)
+			bx, by, er2 := corner(i, j)
+			cx, cy, er3 := corner(i, j+1)
+			dx, dy, er4 := corner(i+1, j+1)
+			// Doesn't print if polygon contains infinite/NaN points
+			if er1 == nil && er2 == nil && er3 == nil && er4 == nil {
+				fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Fprintf(out, "</svg>\n")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 //!-
